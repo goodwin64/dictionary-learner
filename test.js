8233a00767635952e824312c9bdff557de1e46d5
SergeyKovalchuk <sergeyalexkoval@gmail.com>
2017-10-22 22:48:12 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const check = require('./src/index.js');
+const findRotationPoint = require('./src/index.js');
 
 const words1 = [
     'ptolemaic',
@@ -12,9 +12,42 @@ const words1 = [
     'banoffee',
     'engender',
     'karpatka',
+    'othellolagkage'
+];
+
+const words2 = ['gobbledygook'];
+
+const words3 = [
+    'asymptote',
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage'
+];
+
+const words4 = [
     'othellolagkage',
+    'asymptote',
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage'
 ];
 
 it('should return correct rotation index for words1', () => {
   assert.equal(findRotationPoint(words1), 5);
+});
+
+it('should return 0 when you collect only one work in your dictionary', () => {
+  assert.equal(findRotationPoint(words2), 0);
+});
+
+it('should return correct rotation index for words3', () => {
+  assert.equal(findRotationPoint(words3), 0);
+});
+
+it('should return correct rotation index for words4', () => {
+  assert.equal(findRotationPoint(words4), 1);
 });
