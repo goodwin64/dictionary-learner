34789a55651c22e9fc5ce1c45bbda85cc6dcdd93
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-07 16:12:04 +0200

    via simple "reduce"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function findRotationPoint(words) {
-  // your solution here
-}
+    return words.slice(1).reduce((indexOfRotation, word, index) => {
+        if (word.localeCompare(words[index - 1]) === -1) {
+            return index;
+        }
+        return indexOfRotation;
+    }, 0);
+};
